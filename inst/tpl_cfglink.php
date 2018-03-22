15374efaf8d9965779ef77680133d60a80915b89
Fabian <gitcommit@qnote.de>
2018-03-22 17:50:22 +0100

    src-documentation (plus first two files)

--- inst/tpl_cfglink.php
+++ inst/tpl_cfglink.php
@@ -1,3 +1,7 @@
 <?php
+/*DOCSTART
+ * This template is used to create "config_link.php", which is created by the installer and contains a link to the
+ * configuration file.
+ DOCEND*/
 
 define('TCFGFILE', ':cfglink');
