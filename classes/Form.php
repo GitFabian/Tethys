991cb0346f2865133613b3db249a40b9a0599362
Fabian <gitcommit@qnote.de>
2018-03-25 21:37:13 +0200

    Documentation: Form

--- classes/Form.php
+++ classes/Form.php
@@ -6,18 +6,44 @@
  * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
  GPL*/
 /**
- * Class Form
+ * This file contains classes Form, Formfield and children (different types) of Formfield.
+ */
+/**
  *
- * include_once(ROOT_HDD_CORE.'/classes/Form.php');
+ * Class Form
+ * ==========
+ * Example of creating a form in Tethys:
+ *     include_once(ROOT_HDD_CORE.'/classes/Form.php');
+ *     $form = new Form("", "Speichern", "cmd_save_cfglink");
+ *     $form->add_field(new Formfield_text("cfglink", "Konfigurationsdatei", $cfgfile_proposal));
+ *     $page->addHtml($form);
  *
  */
 class Form {
 
+	/**
+	 * $action is an URL that is called on form submission. Can be left empty (same page is called).
+	 * @var string
+	 */
 	private $action;
+	/**
+	 * Form submission method. The submission method is "post" by default, for developers "get".
+	 * @var string "get"|"post"
+	 */
 	private $method;
 	private $buttons = array();
 	private $fields = array();
 
+	/**
+	 * Form constructor.
+	 * @param string      $action is an URL that is called on form submission. Can be left empty (same page is called).
+	 * @param string      $submit_text
+	 *                    Label of the submit button.
+	 * @param string|null $cmd
+	 *                    If set, a hidden key "cmd" is sent on submission.
+	 * @param string|null $method
+	 *                    Form submission method. The submission method is "post" by default, for developers "get".
+	 */
 	public function __construct($action = "", $submit_text = "Absenden", $cmd = null, $method = null) {
 
 		$this->action = $action;
@@ -51,27 +77,50 @@ class Form {
 
 }
 
+/**
+ * Class Formfield
+ * Generic class representing all formfields.
+ */
 class Formfield {
 
 	//Formfield:
 	protected $name;
-	protected $title;
 	protected $value;
 	public $id = null;
 	public $more_params = array();
 
-	//Surrounding div:
+	//Label:
+	protected $title;
 	public $tooltip = "";
+
+	//Surrounding div:
 	public $outer_id = null;
 	public $outer_class = null;
 	public $outer_more_params = array();
 
+	/**
+	 * Formfield constructor.
+	 * @param             $name
+	 * @param string|null $title
+	 *                    If set to null, the fieldname is used as label.
+	 * @param string|null $value
+	 * @param bool        $val_from_request
+	 *                    If set to true, the default value ($value) can be overwritten by the request.
+	 *                    Example: .../myform.php?myvalue=Foo
+	 */
 	function __construct($name, $title = null, $value = null, $val_from_request=true) {
 		$this->name = $name;
+
+		//Title: If set to null, the fieldname is used as label.
 		$this->title = ($title === null ? $name : $title);
+
 		$this->value = $val_from_request?request_value($name, $value):$value;
 	}
 
+	/**
+	 * Generic function is overwritten with the respective HTML by the children.
+	 * @return string
+	 */
 	protected function inner_html() {
 		return "UNSPECIFIED FORMTYPE";
 	}
@@ -92,14 +141,18 @@ class Formfield {
 
 		$title = $tooltip ? "title='" . escape_value_html($tooltip) . "'" : "";
 
-		//@formatter:off
 		return "<div" . $this->getParams_outer() . ">"
 				. "<label $title>$label</label>"
 				. $this->inner_html()
 			. "</div>";
-		//@formatter:on
 	}
 
+	/**
+	 * Every formfield has a name, a value, an id and possibly a list of some other parameters ($more_params).
+	 * This function creates the corresponding HTML-snippet.
+	 * @param bool $value If set to false, the parameter "value" is skipped.
+	 * @return string String to insert into the HTML code.
+	 */
 	protected function getParams_inner($value = true) {
 		$params = $this->more_params;
 
@@ -110,6 +163,9 @@ class Formfield {
 		return html_tag_keyValues($params);
 	}
 
+	/**
+	 * For documentation @see getParams_inner.
+	 */
 	protected function getParams_outer() {
 		$params = $this->outer_more_params;
 
@@ -121,6 +177,10 @@ class Formfield {
 
 }
 
+/**
+ * Class Formfield_hidden
+ * Hidden "input"s can -for example- be used to send commands.
+ */
 class Formfield_hidden extends Formfield {
 
 	public function __construct($name, $value) {
@@ -133,6 +193,10 @@ class Formfield_hidden extends Formfield {
 
 }
 
+/**
+ * Class Formfield_text
+ * A single-line text input field.
+ */
 class Formfield_text extends Formfield {
 
 	public function inner_html() {
@@ -141,6 +205,10 @@ class Formfield_text extends Formfield {
 
 }
 
+/**
+ * Class Formfield_password
+ * Prompt for a password.
+ */
 class Formfield_password extends Formfield {
 
 	public function inner_html() {
@@ -149,6 +217,10 @@ class Formfield_password extends Formfield {
 
 }
 
+/**
+ * Class Formfield_select
+ * An input field to select from predefined values.
+ */
 class Formfield_select extends Formfield {
 
 	private $values;
