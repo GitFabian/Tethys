a819d42ebbd193a8bfe2a61c3d20ea75b0611a3d
Fabian <gitcommit@qnote.de>
2019-02-08 22:26:36 +0100

    Global commands parser. Global command "save".

---
+++ core/Commands.php
@@ -0,0 +1,66 @@
+<?php
+/*GPL
+ * This file is part of the Tethys framework;
+ * Copyright (C) 2014-2019 Fabian Perder (tethys@qnote.de) and contributors
+ * Tethys comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under
+ * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
+ GPL*/
+/**
+ * Core commands
+ * require_once ROOT_HDD_CORE . "/core/Commands.php";
+ */
+namespace core;
+
+use tools\T_Debug;
+
+class Commands {
+
+	public static function handle($cmd){
+		switch ($cmd){
+			case "save":
+				self::cmd_save();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public static function cmd_save(){
+		$page = Page::get_global_page();
+
+		$table = request_value("table");
+		$id = request_value("id");
+
+		if(!$table || !$id){
+			Errors::die_hard("Fehler beim Speichern!");
+			return false;
+		}
+
+		$fields = Database::dbio_info_columns($table);
+		$data = array();
+		foreach ($fields as $key => $dummy){
+			if($key!="id"){
+				$data[$key] = request_value($key);
+			}
+		}
+
+		if(strcasecmp($id,"NEW")==0){
+			$insert_id = Database::insert_assoc($table, $data);
+		}else{
+			$id_escaped = escape_sql($id);
+			$insert_id = Database::update_assoc($table, "`id`='$id_escaped'", $data);
+		}
+		if(!$insert_id){
+			if(Database::get_error_code()===false){
+				$page->addMessageInfo("No changes to $table #$insert_id.");
+				return true;
+			}else{
+				$page->addMessageError("Fehler beim Speichern von $table #$insert_id!<br>".Database::get_error_msg());
+				return false;
+			}
+		}
+
+		$page->addMessageConfirm("Saved $table #$insert_id.");
+		return true;
+	}
+}
